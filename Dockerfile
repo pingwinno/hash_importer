25cca262aa4d4d10cc060386b1834e3f7c3df754
max <pingwinno@gmail.com>
2024-05-17 07:02:28 +0300

    Add requirements.txt, git ignore, Dockerfile Add path options and ability to process multiple files

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:3-alpine
+
+WORKDIR /usr/src/app
+
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY hash_importer.py /usr/src/app
+
+CMD [ "python", "./hash_importer.py" ]
--- hash_importer.py
+++ hash_importer.py
@@ -8,50 +8,58 @@ import imagehash
 from PIL import Image
 from bs4 import BeautifulSoup
 
-page_data = {}
-
 parser = argparse.ArgumentParser(
     prog='difference_parser',
     description='Show top lines from each file')
-parser.add_argument('filenames', nargs='+')
+parser.add_argument('--id', type=str)
+parser.add_argument('--folder', type=str)
 args = parser.parse_args()
 
-chat_id = args.filenames[0]
+chat_id = args.id
+args.folder = os.path.join('chat_dump/', args.folder)
+counter = 0
 
 
 def has_class_but_no_id(tag):
     return tag.has_attr('div', class_='photo_wrap clearfix pull_left')
 
 
-html_doc = open("ChatExport_2024-05-15/messages.html")
-messages = BeautifulSoup(html_doc, 'html.parser')
-messages_list = messages.find_all('a', class_='photo_wrap')
-for message in messages_list:
-    message_id = ''
-    photo_ref = message['href']
-    print(photo_ref)
-    file = open('ChatExport_2024-05-15/' + photo_ref, 'rb')
-    image_hash = imagehash.dhash(Image.open(file))
-    hash_key = str(image_hash)
-    print(hash_key)
-    if message.parent.parent.parent.has_attr('id'):
-        message_id = message.parent.parent.parent['id'][7:]
-    else:
-        message_id = message.parent.parent.parent.parent['id'][7:]
-    image_key = str(image_hash) + '^' + chat_id
-    print(message_id)
-    page_data[image_key] = message_id
+def parse_html(html_file):
+    zipfile_template = f'chat_dump/hash_data-{counter}.zip'
+    page_data = {}
+    html_doc = open(html_file)
+    messages = BeautifulSoup(html_doc, 'html.parser')
+    messages_list = messages.find_all('a', class_='photo_wrap')
+    for message in messages_list:
+        photo_ref = message['href']
+        print(photo_ref)
+        file = open(os.path.join(args.folder, photo_ref), 'rb')
+        image_hash = imagehash.dhash(Image.open(file))
+        hash_key = str(image_hash)
+        print(hash_key)
+        if message.parent.parent.parent.has_attr('id'):
+            message_id = message.parent.parent.parent['id'][7:]
+        else:
+            message_id = message.parent.parent.parent.parent['id'][7:]
+        image_key = str(image_hash) + '^' + chat_id
+        print(message_id)
+        page_data[image_key] = message_id
 
-page_data = dict(reversed(list(page_data.items())))
-if os.path.exists('hash_data.zip'):
-    os.rename('hash_data.zip', 'hash_data.zip:' + str(datetime.datetime.now()))
+    page_data = dict(reversed(list(page_data.items())))
+    if os.path.exists(zipfile_template):
+        os.rename(zipfile_template, zipfile_template + ':' + str(datetime.datetime.now()))
 
-with ZipFile('hash_data.zip', 'x') as myzip:
-    ZipFile.writestr(myzip, 'hash_data.json', data=json.dumps(page_data))
+    with ZipFile(zipfile_template, 'x') as myzip:
+        ZipFile.writestr(myzip, 'hash_data.json', data=json.dumps(page_data))
 
-with open('hash_data.zip', 'rb') as t:
-    with ZipFile(t, 'r') as json_data:
-        json_str = json.load(json_data.open('hash_data.json'))
-for k, v in json_str.items():
-    print(k + "   " + v)
-print(len(json_str))
+    with open(zipfile_template, 'rb') as t:
+        with ZipFile(t, 'r') as json_data:
+            json_str = json.load(json_data.open('hash_data.json'))
+
+    print(zipfile_template + ' has ' + str(len(json_str)) + ' elements')
+
+
+for file in os.listdir(args.folder):
+    if file.endswith(".html"):
+        parse_html(os.path.join(args.folder, file))
+        counter += 1
